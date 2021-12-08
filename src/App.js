9db5f7f61bd8a3bf3ff8cb4400035d08ce1f82b7
Jonathan Bunnell <jbunnell@rams.colostate.edu>
2021-12-08 12:46:48 -0500

    add NavBar Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./Components/Navbar"
 
 function App() {
   return (
-    <h2>hi</h2>
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
